2a139d7ae2b61a57dc22fdf044c075886559cbc4
Erik Marks <rekmarks@protonmail.com>
2024-07-19 17:56:38 +0200

    chore: Remove extraneous comments

--- packages/germs/.eslintrc.js
+++ packages/germs/.eslintrc.js
@@ -2,20 +2,4 @@ module.exports = {
   extends: ['../../.eslintrc.js'],
 
   ignorePatterns: ['**/shims/*.mjs'],
-
-  // overrides: [
-  //   {
-  //     files: ['src/**/*.js'],
-  //     globals: { chrome: 'readonly', clients: 'readonly' },
-  //   },
-
-  //   {
-  //     files: ['vite.config.mts'],
-  //     parserOptions: {
-  //       sourceType: 'module',
-  //       tsconfigRootDir: __dirname,
-  //       project: ['./tsconfig.scripts.json'],
-  //     },
-  //   },
-  // ],
 };
