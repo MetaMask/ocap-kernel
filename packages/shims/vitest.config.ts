27590417c9ed762a02706d290abafa1e77550a63
grypez <143971198+grypez@users.noreply.github.com>
2024-08-20 23:47:41 -0400

    test(shims): Configure test setup

--- packages/shims/vitest.config.ts
+++ packages/shims/vitest.config.ts
@@ -1,9 +1,20 @@
 // eslint-disable-next-line spaced-comment
 /// <reference types="vitest" />
 
+import { defineConfig, mergeConfig } from 'vite';
 import { getDefaultConfig } from '../../vitest.config.packages.js';
 
-const config = getDefaultConfig();
+
+const config = mergeConfig(
+  getDefaultConfig(),
+  defineConfig({
+    test: {
+      exclude: ['endoify-*shimmed.test.*'],
+      setupFiles: '../test-utils/src/env/mock-endo.ts',
+    },
+  }),
+);
+
 // @ts-expect-error We can and will delete this.
 delete config.test.coverage.thresholds;
 export default config;
