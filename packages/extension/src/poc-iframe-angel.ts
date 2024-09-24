99a3ae4367b59c15081ed86a5cd4046a7db111d0
grypez <143971198+grypez@users.noreply.github.com>
2024-09-23 19:42:53 -0500

    remove unnecessary delay

--- packages/extension/src/poc-iframe-angel.ts
+++ packages/extension/src/poc-iframe-angel.ts
@@ -1,5 +1,4 @@
 import { receiveMessagePort } from '@ocap/streams';
-import { delay } from '@ocap/test-utils';
 import { THE_ANGEL } from './poc-constants.js';
 
 main().catch(console.error);
@@ -9,8 +8,6 @@ main().catch(console.error);
  */
 async function main(): Promise<void> {
   const port = await receiveMessagePort();
-
-  await delay(100);
 
   console.log(THE_ANGEL);
   port.postMessage(THE_ANGEL);
