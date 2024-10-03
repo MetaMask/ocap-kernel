0d2d329c28e250f5d04532e134d084729d27ae0a
grypez <143971198+grypez@users.noreply.github.com>
2024-10-03 13:23:51 -0500

    short circuit element tests

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -18,10 +18,11 @@ export const isPrimitive = (value: unknown): value is Primitive =>
 export const isTypedArray = <ElementType>(
   value: unknown,
   isElement: TypeGuard<ElementType>,
-): value is ElementType[] => Array.isArray(value) && value.every(isElement);
+): value is ElementType[] =>
+  Array.isArray(value) && !value.some((ele) => !isElement(ele));
 
 export const isTypedObject = <ValueType>(
   value: unknown,
   isValue: TypeGuard<ValueType>,
 ): value is { [Key in keyof object]: ValueType } =>
-  isObject(value) && Object.values(value).every(isValue);
+  isObject(value) && !Object.values(value).some((val) => !isValue(val));
