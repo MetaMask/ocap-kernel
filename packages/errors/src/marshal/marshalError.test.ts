8d2b1c4e33ceb9f814db491a26fc591d63f153ec
grypez <143971198+grypez@users.noreply.github.com>
2024-11-06 15:39:23 -0600

    test(errors): Cover unlikely if branches

--- packages/errors/src/marshal/marshalError.test.ts
+++ packages/errors/src/marshal/marshalError.test.ts
@@ -1,3 +1,4 @@
+import { BaseError } from 'src/BaseError.js';
 import { describe, it, expect } from 'vitest';
 
 import { marshalError } from './marshalError.js';
@@ -13,6 +14,18 @@ describe('marshalError', () => {
         [ErrorSentinel]: true,
         message: 'foo',
         stack: expect.any(String),
+      }),
+    );
+  });
+
+  it('should marshal an error without a stack', () => {
+    const error = new Error('foo');
+    delete error.stack;
+    const marshaledError = marshalError(error);
+    expect(marshaledError).toStrictEqual(
+      expect.objectContaining({
+        [ErrorSentinel]: true,
+        message: 'foo',
       }),
     );
   });
@@ -49,7 +62,7 @@ describe('marshalError', () => {
     );
   });
 
-  it('should marshal an ocap error', () => {
+  it('should marshal an ocap error with data', () => {
     const error = new VatNotFoundError('v1');
     const marshaledError = marshalError(error);
     expect(marshaledError).toStrictEqual(
@@ -59,6 +72,21 @@ describe('marshalError', () => {
         stack: expect.any(String),
         code: ErrorCode.VatNotFound,
         data: { vatId: 'v1' },
+      }),
+    );
+  });
+
+  it('should marshal an ocap error without data', () => {
+    const error = new BaseError(ErrorCode.StreamReadError, 'message', {
+      stack: 'stack',
+    });
+    const marshaledError = marshalError(error);
+    expect(marshaledError).toStrictEqual(
+      expect.objectContaining({
+        [ErrorSentinel]: true,
+        message: 'message',
+        stack: expect.any(String),
+        code: ErrorCode.StreamReadError,
       }),
     );
   });
