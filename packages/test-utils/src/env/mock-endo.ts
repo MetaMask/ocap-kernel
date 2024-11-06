5279f05c89720df9aed0d1327398b78d79575f96
grypez <143971198+grypez@users.noreply.github.com>
2024-11-06 15:43:27 -0600

    Add more properties to mock-endo

--- packages/test-utils/src/env/mock-endo.ts
+++ packages/test-utils/src/env/mock-endo.ts
@@ -5,5 +5,31 @@ import { vi } from 'vitest';
 
 globalThis.lockdown = (): void => undefined;
 globalThis.harden = vi.fn(<Value>(value: Value): Readonly<Value> => value);
+globalThis.assert = vi.fn(
+  (..._: unknown[]): boolean => true,
+) as unknown as typeof globalThis.assert;
+for (const prop of [
+  'typeof',
+  'error',
+  'fail',
+  'equal',
+  'string',
+  'note',
+  'details',
+  'Fail',
+  'quote',
+  'makeAssert',
+]) {
+  Object.defineProperty(globalThis.assert, prop, { value: vi.fn() });
+}
+globalThis.HandledPromise = Promise as typeof globalThis.HandledPromise;
+Object.defineProperty(globalThis, 'Compartment', {
+  value: class Compartment {
+    evaluate(expr: string): unknown {
+      // eslint-disable-next-line no-eval
+      return eval(expr);
+    }
+  },
+});
 
 export {};
