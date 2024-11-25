9630f6bcf896b608ce3a69cdb6f5f97193cd1351
Dimitris Marlagkoutsos <info@sirtimid.com>
2024-11-25 16:48:59 +0000

    fix jsdoc

--- packages/extension/src/panel/shared.ts
+++ packages/extension/src/panel/shared.ts
@@ -3,10 +3,10 @@ import { makeLogger } from '@ocap/utils';
 export const logger = makeLogger('[Kernel Panel]');
 
 /**
- * Validates a URL
+ * Validates a bundle URL.
  *
- * @param url - The URL to validate
- * @returns Whether the URL is valid
+ * @param url - The bundle URL to validate
+ * @returns Whether the URL is a valid bundle URL
  */
 export function isValidBundleUrl(url?: string): boolean {
   if (!url) {
