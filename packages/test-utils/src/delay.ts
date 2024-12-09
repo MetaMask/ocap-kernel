59803dde0693e16d6b58225295b482b0eed3312f
Erik Marks <rekmarks@protonmail.com>
2024-12-09 14:49:01 -0800

    refactor: Move delay func to prod utils, use where possible

--- packages/test-utils/src/delay.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Delay execution by the specified number of milliseconds.
- *
- * @param ms - The number of milliseconds to delay.
- * @returns A promise that resolves after the specified delay.
- */
-export const delay = async (ms = 1): Promise<void> =>
-  new Promise((resolve) => setTimeout(resolve, ms));
--- packages/test-utils/src/index.ts
+++ packages/test-utils/src/index.ts
@@ -1,3 +1,2 @@
-export { delay } from './delay.js';
 export { makeErrorMatcherFactory } from './errors.js';
 export { makePromiseKitMock } from './promise-kit.js';
