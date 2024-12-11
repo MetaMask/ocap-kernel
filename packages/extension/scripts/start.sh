a4fb314d38678f35767569243397dfb277109555
grypez <143971198+grypez@users.noreply.github.com>
2024-12-10 18:06:23 -0600

    swap jobs

--- packages/extension/scripts/start.sh
+++ packages/extension/scripts/start.sh
@@ -4,17 +4,17 @@ set -x
 set -e
 set -o pipefail
 
-yarn build:vite:dev --watch &
-BUILD_PID=$!
+yarn ocap start "./src/vats" &
+OCAP_PID=$!
 
 function cleanup() {
   # Kill the build if it's still running
-  if kill -0 $BUILD_PID 2>/dev/null; then
-    kill $BUILD_PID
+  if kill -0 $OCAP_PID 2>/dev/null; then
+    kill $OCAP_PID
   fi
 }
 
-# Ensure we always close the builder
+# Ensure we always close the ocap cli
 trap cleanup EXIT
 
-yarn ocap start "./src/vats"
+yarn build:vite:dev --watch
