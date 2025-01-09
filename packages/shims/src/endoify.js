bcc450a8cefc2e9bb8bf6431e202b846e002ddaa
Dimitris Marlagkoutsos <info@sirtimid.com>
2025-01-09 19:33:14 +0200

    revert lcokdown changes

--- packages/shims/src/endoify.js
+++ packages/shims/src/endoify.js
@@ -3,15 +3,10 @@ import '@endo/eventual-send/shim.js';
 
 lockdown({
   consoleTaming: 'unsafe',
-  // errorTaming: 'unsafe',
+  errorTaming: 'unsafe',
   mathTaming: 'unsafe',
   dateTaming: 'unsafe',
   domainTaming: 'unsafe',
   overrideTaming: 'severe',
   stackFiltering: 'verbose',
-  // TODO: fix these
-  errorTrapping: 'report',
-  reporting: 'console',
-  unhandledRejectionTrapping: 'report',
-  errorTaming: 'unsafe-debug',
 });
