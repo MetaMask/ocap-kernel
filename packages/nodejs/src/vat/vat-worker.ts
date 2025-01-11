6df657b376b16b324c305669baafa3b09bf2f842
grypez <143971198+grypez@users.noreply.github.com>
2025-01-10 18:24:54 -0600

    align nodejs vat-worker with extension vat iframe

--- packages/nodejs/src/vat/vat-worker.ts
+++ packages/nodejs/src/vat/vat-worker.ts
@@ -1,36 +1,38 @@
 import '@ocap/shims/endoify';
 
-import { VatSupervisor } from '@ocap/kernel';
+import { isVatCommand, VatSupervisor } from '@ocap/kernel';
 import type { VatCommand, VatCommandReply } from '@ocap/kernel';
 import { NodeWorkerMultiplexer } from '@ocap/streams';
 import { makeLogger } from '@ocap/utils';
 import { parentPort } from 'node:worker_threads';
 
 import { makeSQLKVStore } from '../kernel/sqlite-kv-store.js';
+const vatId = process.env.NODE_VAT_ID
 
 // eslint-disable-next-line n/no-process-env
-const logger = makeLogger(`[vat-worker (${process.env.NODE_VAT_ID})]`);
+const logger = makeLogger(`[vat-worker ${vatId}]`);
 
 main().catch(logger.error);
 
 /**
  * The main function for the iframe.
  */
-async function main(): Promise<void> {
+export async function main(): Promise<void> {
+  logger.debug('started main');
+
   if (!parentPort) {
     const errMsg = 'Expected to run in Node Worker with parentPort.';
     logger.error(errMsg);
     throw new Error(errMsg);
   }
   const multiplexer = new NodeWorkerMultiplexer(parentPort, 'vat');
-  console.debug('premulti');
   multiplexer.start().catch(logger.error);
-  console.debug('postmulti');
   const commandStream = multiplexer.createChannel<VatCommand, VatCommandReply>(
     'command',
+    isVatCommand,
   );
-  // eslint-disable-next-line no-void
-  void new VatSupervisor({
+
+  const supervisor = new VatSupervisor({
     id: 'iframe',
     commandStream,
     makeKVStore: makeSQLKVStore,
