7fc6d8561dded9f28e1fa9593ca3f13709a74ce3
grypez <143971198+grypez@users.noreply.github.com>
2025-01-14 16:08:11 -0600

    fix script

--- packages/nodejs/scripts/build-sqlite3.sh
+++ packages/nodejs/scripts/build-sqlite3.sh
@@ -7,14 +7,14 @@ set -o pipefail
 dir=$(pwd)
 
 package_root="$(dirname "$0")/.."
-cd $package_root
+cd "$package_root"
 package_root=$(pwd)
 monorepo_root="$package_root/../.."
 
-while getopts ":ab:" opt; do
+while getopts ":ab:"; do
   case $OPTARG in
-    f) force=1;;
-    \?) echo "Invalid option: -$OPTARG"; exit 1;;
+    f) force=1 ;;
+    \?) echo "Invalid option: -$OPTARG"; exit 1 ;;
   esac
 done
 
@@ -26,15 +26,15 @@ fi
 echo "Building better-sqlite3 bindings."
 
 # build better-sqlite at the monorepo root
-cd $monorepo_root
+cd "$monorepo_root"
 cd node_modules/better-sqlite3
 yarn build-release
 
 # move back to the source folder
-cd $package_root
+cd "$package_root"
 
 # copy the build to this package
 mkdir -p node_modules/better-sqlite3/build/
 cp -r ../../node_modules/better-sqlite3/build/Release/ node_modules/better-sqlite3/build/
 
-cd $dir
+cd "$dir"
