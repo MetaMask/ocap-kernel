fed6fe8fd66266755385a065bffe5a02d50bc951
Dimitris Marlagkoutsos <info@sirtimid.com>
2025-02-03 20:11:38 +0100

    chore: update thresholds

--- vitest.config.ts
+++ vitest.config.ts
@@ -68,10 +68,10 @@ export default defineConfig({
           lines: 100,
         },
         'packages/extension/**': {
-          statements: 73.76,
+          statements: 73.63,
           functions: 78.97,
           branches: 70.86,
-          lines: 73.8,
+          lines: 73.67,
         },
         'packages/kernel/**': {
           statements: 48.54,
