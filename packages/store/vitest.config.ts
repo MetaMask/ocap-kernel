ba6940c44aeb50416d4d3ac2e5f5f7d87b8bff6a
Dimitris Marlagkoutsos <info@sirtimid.com>
2025-02-06 20:30:37 +0100

    Fix store vitest config

--- packages/store/vitest.config.ts
+++ packages/store/vitest.config.ts
@@ -1,14 +1,18 @@
-import path from 'node:path';
-import { defineProject, mergeConfig } from 'vitest/config';
+import { mergeConfig } from '@ocap/test-utils/vitest-config';
+import path from 'path';
+import { defineConfig, defineProject } from 'vitest/config';
 
 import defaultConfig from '../../vitest.config.js';
 
-export default mergeConfig(
-  defaultConfig,
-  defineProject({
-    test: {
-      name: 'store',
-      setupFiles: path.resolve(__dirname, '../shims/src/endoify.js'),
-    },
-  }),
-);
+export default defineConfig((args) => {
+  return mergeConfig(
+    args,
+    defaultConfig,
+    defineProject({
+      test: {
+        name: 'store',
+        setupFiles: path.resolve(__dirname, '../shims/src/endoify.js'),
+      },
+    }),
+  );
+});
