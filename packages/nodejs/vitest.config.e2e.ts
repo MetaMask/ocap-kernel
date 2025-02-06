28c46b7c7b4f3d59e6424a09df58fe612513904a
Dimitris Marlagkoutsos <info@sirtimid.com>
2025-02-06 16:34:49 +0100

    fix test

--- packages/nodejs/vitest.config.e2e.ts
+++ packages/nodejs/vitest.config.e2e.ts
@@ -1,15 +1,19 @@
-import { defineConfig, mergeConfig } from 'vite';
+import { mergeConfig } from '@ocap/test-utils/vitest-config';
+import { defineConfig, defineProject } from 'vitest/config';
 
 import defaultConfig from '../../vitest.config.js';
 
-export default mergeConfig(
-  defaultConfig,
-  defineConfig({
-    test: {
-      name: 'nodejs:e2e',
-      pool: 'forks',
-      include: ['./test/e2e/**/*.test.ts'],
-      exclude: ['./src/**/*'],
-    },
-  }),
-);
+export default defineConfig((args) => {
+  return mergeConfig(
+    args,
+    defaultConfig,
+    defineProject({
+      test: {
+        name: 'nodejs:e2e',
+        pool: 'forks',
+        include: ['./test/e2e/**/*.test.ts'],
+        exclude: ['./src/**/*'],
+      },
+    }),
+  );
+});
