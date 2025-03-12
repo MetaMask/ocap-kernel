d5ee80ababf948c1421691ce2e252b9203aed1aa
grypez <143971198+grypez@users.noreply.github.com>
2025-03-12 08:47:11 -0400

    misc

--- packages/nodejs/src/demo/logger.ts
+++ packages/nodejs/src/demo/logger.ts
@@ -1,3 +1,10 @@
+export type Loggerish = {
+  label: string;
+  log: (...content: unknown[]) => void;
+  debug: (...content: unknown[]) => void;
+  error: (...content: unknown[]) => void;
+};
+
 /**
  * Temporary replacement for `@ocap/utils` logger pending @metamask/superstruct
  *
@@ -9,19 +16,41 @@
 export const makeLogger = (args: {
   label: string;
   verbose?: boolean;
-}): {
-  label: string;
-  log: (...content: unknown[]) => void;
-  debug: (...content: unknown[]) => void;
-  error: (...content: unknown[]) => void;
-} => {
+}): Loggerish => {
   const { label, verbose } = args;
   return {
     label,
     log: (...content: unknown[]) => console.log(label, ...content),
     debug: verbose
-      ? ((...content: unknown[]) => console.debug(label, ...content))
+      ? (...content: unknown[]) => console.debug(label, ...content)
       : () => undefined,
     error: (...content: unknown[]) => console.error(label, ...content),
   };
+};
+
+export type StreamLogger<Content = unknown> = (
+  stream: AsyncIterable<Content>,
+) => Promise<string>;
+
+/**
+ * Make a stream consumer which logs intermediate progress to the writer
+ * and promises the accumulated content upon stream completion.
+ *
+ * @param writer - Where to write the intermediate content.
+ * @returns A promise for the accumulated content from the stream.
+ */
+export const makeStreamLogger = <Content = unknown>(
+  writer: (content: unknown) => void,
+): StreamLogger<Content> => {
+  const streamLogger = async (
+    stream: AsyncIterable<Content>,
+  ): Promise<string> => {
+    let accumulatedContent: string = '';
+    for await (const content of stream) {
+      accumulatedContent += content;
+      writer(content);
+    }
+    return accumulatedContent;
+  };
+  return streamLogger;
 };
--- packages/nodejs/src/vat/powers/make-powers.ts
+++ packages/nodejs/src/vat/powers/make-powers.ts
@@ -1,24 +1,26 @@
-import makeLoadDocument from "./load-document";
-import makeGetOllama from "./ollama";
-import makeGetVectorStore from "./vector-store";
-
-import type { Args as LoadDocumentArgs } from "./load-document.js";
+import makeLoadDocument from './load-document';
+import type { Args as LoadDocumentArgs } from './load-document.js';
+import makeGetOllama from './ollama';
+import makeGetVectorStore from './vector-store';
 
 type Args = {
-  loadDocument?: LoadDocumentArgs,
+  loadDocument?: LoadDocumentArgs;
   ollama?: {
-    host: string,
-  },
+    host: string;
+  };
   vectorStore?: {
-    host: string,
-    model: string,
-  },
-}
+    host: string;
+    model: string;
+  };
+};
 
 /**
  * Make the powers for a vat.
- * 
+ *
  * @param param0 - The args.
+ * @param param0.loadDocument - The loadDocument power.
+ * @param param0.ollama - The ollama power.
+ * @param param0.vectorStore - The vectorStore power.
  * @returns A Powers object.
  */
 export default async function makePowers({
@@ -29,25 +31,26 @@ export default async function makePowers({
   let powers = {
     setInterval,
     clearInterval,
+    getStdout: () => process.stdout,
   } as Record<string, unknown>;
 
   if (loadDocument) {
     powers = {
       ...powers,
       loadDocument: makeLoadDocument(loadDocument),
-    }
+    };
   }
   if (ollama) {
     powers = {
       ...powers,
       ollama: await makeGetOllama(ollama),
-    }
+    };
   }
   if (vectorStore) {
     powers = {
       ...powers,
       getVectorStore: makeGetVectorStore(vectorStore),
-    }
+    };
   }
 
   return powers;
