5fcadcea833b51f1eaf08fbeb93ce79e58bd2842
grypez <143971198+grypez@users.noreply.github.com>
2025-03-19 11:20:36 -0400

    extend extension e2e timeout

--- packages/extension/playwright.config.ts
+++ packages/extension/playwright.config.ts
@@ -3,6 +3,9 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './test/e2e',
   retries: 1,
+  expect: {
+    timeout: 15000,
+  },
   use: {
     trace: 'on-first-retry',
   },
