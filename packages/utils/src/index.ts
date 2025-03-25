c5a62127c0bbb6c4c866e13cf403ad0027395c01
Erik Marks <rekmarks@protonmail.com>
2025-03-25 13:17:57 -0700

    refactor: Move PromiseCallbacks type into utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,6 +2,6 @@ export type { Logger } from './logger.ts';
 export { makeLogger } from './logger.ts';
 export { delay, makeCounter } from './misc.ts';
 export { stringify } from './stringify.ts';
-export type { TypeGuard, ExtractGuardType } from './types.ts';
+export type { TypeGuard, ExtractGuardType, PromiseCallbacks } from './types.ts';
 export { isPrimitive, isTypedArray, isTypedObject } from './types.ts';
 export { fetchValidatedJson } from './fetchValidatedJson.ts';
