9ff920015fefeef4567845f2f0dc1aed521daf10
Erik Marks <rekmarks@protonmail.com>
2025-05-21 10:44:20 -0700

    refactor: Make some improvements to the kernel worker build scripts

--- packages/kernel-browser-runtime/scripts/build-kernel-worker.js
+++ packages/kernel-browser-runtime/scripts/build-kernel-worker.js
@@ -1,30 +1,36 @@
 // @ts-check
 
 import { build } from 'esbuild';
+import hasFlag from 'has-flag';
 import fs from 'node:fs/promises';
 import path from 'node:path';
+
+const isDev = hasFlag('dev') || hasFlag('development');
 
 // We will only run this where it's available, but ESLint doesn't know that
 // eslint-disable-next-line n/no-unsupported-features/node-builtins
 const rootDir = path.resolve(import.meta.dirname, '..');
-const endoify = path.resolve(rootDir, '../kernel-shims/dist/endoify.js');
+const endoifyFile = path.resolve(rootDir, '../kernel-shims/dist/endoify.js');
 const outDir = path.resolve(rootDir, 'dist/kernel-worker');
 const outfile = path.resolve(outDir, 'index.mjs');
+
+await fs.rm(outDir, { recursive: true, force: true });
 
 await build({
   entryPoints: [path.resolve(rootDir, 'src/kernel-worker/index.ts')],
   outfile,
   // Prepend the endoify shim to the output file
   banner: {
-    js: await fs.readFile(endoify, 'utf8'),
+    js: await fs.readFile(endoifyFile, 'utf8'),
   },
-  sourcemap: 'inline',
-  minify: true,
   bundle: true,
   format: 'esm',
   platform: 'browser',
-  // This file is dynamically imported in Node.js only
-  external: ['*/gc-engine.ts'],
+  tsconfig: path.resolve(rootDir, 'tsconfig.build.json'),
+  minify: !isDev,
+  sourcemap: isDev ? 'inline' : false,
+  // This file is dynamically imported in the ocap-kernel package in Node.js only
+  external: ['./gc-engine.mjs'],
 });
 
 // @sqlite.org/sqlite-wasm fetches certain files at runtime, and esbuild doesn't copy
