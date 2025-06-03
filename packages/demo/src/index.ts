e9313776497ea8b9569ab6af6a3f094cc887ebb9
grypez <143971198+grypez@users.noreply.github.com>
2025-06-03 10:08:35 -0500

    test(demo): Minimal testing

--- packages/demo/src/index.ts
+++ packages/demo/src/index.ts
@@ -3,14 +3,7 @@ import '@metamask/kernel-shims/endoify';
 import cli from './cli.ts';
 import { commands } from './commands.ts';
 
-cli(process.argv, commands)
-  .then(() => {
-    // TODO: make this an exception by convention
-    // eslint-disable-next-line n/no-process-exit
-    process.exit(0);
-  })
-  .catch((error) => {
-    console.error(error);
-    // eslint-disable-next-line n/no-process-exit
-    process.exit(1);
-  });
+cli(process.argv, commands).catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
