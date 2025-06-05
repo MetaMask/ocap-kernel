01f2097baeaa0cbed7aa23e5bdb2e6a7cbb15644
grypez <143971198+grypez@users.noreply.github.com>
2025-06-05 12:08:05 -0500

    feat(demo): 05 delegation

---
+++ packages/demo/demos/05-delegation/delegator.js
@@ -0,0 +1,39 @@
+/**
+ * Example 05: Delegation.
+ * -----------------------
+ * This vat's root object declares a work method that delegates to a list of
+ * workers, which may themselves delegate to other workers or simply perform
+ * the work individually.
+ *
+ * @see bootstrap.js for the assignment of the delegation tree.
+ * @see delegatee.js for the nodes of the tree.
+ */
+
+import { E, Far } from '@endo/far';
+
+import { divide, compute, publish } from './labor.js';
+
+export function buildRootObject(_, { delegates = [] }) {
+  // A worthy delegator remembers that they too can work.
+  const workers = [
+    {
+      work(start, end) {
+        const results = compute(start, end);
+        return publish(start, end, results);
+      },
+    },
+  ];
+
+  return Far('root', {
+    getDelegates: () => delegates,
+    addWorker: (worker) => workers.push(worker),
+
+    async work(start, end) {
+      return Promise.all(
+        divide(start, end, workers.length).map((subrange, i) =>
+          E(workers[i]).work(...subrange),
+        ),
+      ).then((results) => publish(start, end, results.flat()));
+    },
+  });
+}
