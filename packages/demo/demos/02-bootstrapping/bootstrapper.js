4436bb40ad9a8ef0f8e6874ea18a96027751c061
grypez <143971198+grypez@users.noreply.github.com>
2025-06-11 10:22:48 -0500

    02: refinement

--- packages/demo/demos/02-bootstrapping/bootstrapper.js
+++ packages/demo/demos/02-bootstrapping/bootstrapper.js
@@ -2,14 +2,24 @@ import { E, Far } from '@endo/far';
 
 export function buildRootObject() {
   return Far('root', {
+    /**
+     * Bootstrapping takes place after every vat's root object has been built.
+     * This is the first opportunity vats in the cluster have to contact one
+     * another.
+     *
+     * @param {Record<string, object>} vats - A record of root objects, named
+     * according to their entries in the cluster config file, e.g.
+     * @param {object} vats.alice - Alice's root object.
+     * @param {object} vats.bob - Bob's root object.
+     */
     async bootstrap({ alice, bob }) {
-      // Say hello here.
+      // Say our hellos.
       await E(alice).hello().then(console.log);
       await E(bob).hello().then(console.log);
 
-      // Say goodbye over there.
-      await E(alice).goodbye();
-      await E(bob).goodbye();
+      // Then our goodbyes.
+      await E(alice).goodbye().then(console.log);
+      await E(bob).goodbye().then(console.log);
     },
   });
 }
