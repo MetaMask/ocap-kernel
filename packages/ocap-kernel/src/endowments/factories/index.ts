56e80a8381349f76baccab134d8d0219cb2d1ad9
grypez <143971198+grypez@users.noreply.github.com>
2025-06-23 09:55:27 -0400

    feat(ocap-kernel): Add makeOcapUrl endowment

--- packages/ocap-kernel/src/endowments/factories/index.ts
+++ packages/ocap-kernel/src/endowments/factories/index.ts
@@ -1,8 +1,10 @@
+import * as makeOcapUrl from './make-ocap-url.ts';
 import * as makeRevoker from './make-revoker.ts';
 import * as scry from './scry.ts';
 import type { EndowmentDefinition } from '../types.ts';
 
 const endowmentDefinitions = {
+  makeOcapUrl,
   makeRevoker,
   scry,
 } as const satisfies Record<string, EndowmentDefinition>;
