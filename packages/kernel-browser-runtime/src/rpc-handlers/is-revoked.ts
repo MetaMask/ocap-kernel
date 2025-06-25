1741f7df622578149d69fb3a7694ee6ddae88395
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2025-06-25 10:59:10 -0700

    refactor: Fix isRevoked RPC method result type (#552)
    Replaces the `[boolean]` result type of the `isRevoked` RPC method with
    `boolean` by means of casting the method specification. This is
    necessary because using the `boolean()` struct results in `Struct<true,
    unknown> | Struct<false, unknown>`.

--- packages/kernel-browser-runtime/src/rpc-handlers/is-revoked.ts
+++ packages/kernel-browser-runtime/src/rpc-handlers/is-revoked.ts
@@ -1,16 +1,17 @@
 import type { MethodSpec, Handler } from '@metamask/kernel-rpc-methods';
 import type { Kernel, KRef } from '@metamask/ocap-kernel';
-import { string, object, boolean, tuple } from '@metamask/superstruct';
+import { string, object, boolean } from '@metamask/superstruct';
 
 /**
  * Check if a kernel object has been revoked.
  */
-export const isRevokedSpec: MethodSpec<'isRevoked', { kref: KRef }, [boolean]> =
-  {
-    method: 'isRevoked',
-    params: object({ kref: string() }), // KRef
-    result: tuple([boolean()]),
-  };
+export const isRevokedSpec: MethodSpec<'isRevoked', { kref: KRef }, boolean> = {
+  method: 'isRevoked',
+  params: object({ kref: string() }), // KRef
+  result: boolean(),
+  // Using the boolean struct results in `Struct<true, unknown> | Struct<false, unknown>`,
+  // which is not what we want.
+} as MethodSpec<'isRevoked', { kref: KRef }, boolean>;
 
 export type IsRevokedHooks = {
   kernel: Pick<Kernel, 'isRevoked'>;
@@ -19,12 +20,12 @@ export type IsRevokedHooks = {
 export const isRevokedHandler: Handler<
   'isRevoked',
   { kref: KRef },
-  [boolean],
+  boolean,
   IsRevokedHooks
 > = {
   ...isRevokedSpec,
   hooks: { kernel: true },
-  implementation: ({ kernel }, { kref }): [boolean] => {
-    return [kernel.isRevoked(kref)];
+  implementation: ({ kernel }, { kref }): boolean => {
+    return kernel.isRevoked(kref);
   },
 };
