5bf8276b60e4220c4753aa2669a192d69c31878e
grypez <143971198+grypez@users.noreply.github.com>
2025-06-26 15:56:50 -0400

    feat(ocap-kernel): console -> logger migration in services

--- packages/ocap-kernel/src/VatSupervisor.ts
+++ packages/ocap-kernel/src/VatSupervisor.ts
@@ -251,7 +251,9 @@ export class VatSupervisor {
       WeakRef,
       FinalizationRegistry,
       waitUntilQuiescent,
-      gcAndFinalize: makeGCAndFinalize(),
+      gcAndFinalize: makeGCAndFinalize(
+        this.#logger.subLogger({ tags: ['gc'] }),
+      ),
       meterControl: makeDummyMeterControl(),
     });
 
