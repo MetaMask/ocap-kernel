98447bccea47012089a165334ed129b65542a735
grypez <143971198+grypez@users.noreply.github.com>
2025-07-02 12:45:57 -0400

    feat(ocap-kernel): Throw if subcluster launching fails

--- packages/ocap-kernel/src/Kernel.ts
+++ packages/ocap-kernel/src/Kernel.ts
@@ -397,7 +397,16 @@ export class Kernel {
     }
     const bootstrapRoot = rootIds[config.bootstrap];
     if (bootstrapRoot) {
-      return this.queueMessage(bootstrapRoot, 'bootstrap', [roots, services]);
+      return this.queueMessage(bootstrapRoot, 'bootstrap', [
+        roots,
+        services,
+      ]).then((result) => {
+        const unserialized = kunser(result);
+        if (unserialized instanceof Error) {
+          throw unserialized;
+        }
+        return result;
+      });
     }
     return undefined;
   }
