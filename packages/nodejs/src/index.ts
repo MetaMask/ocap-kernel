015ed3be90e24d14cf4b696e8b7f6206dc00afba
grypez <143971198+grypez@users.noreply.github.com>
2025-07-21 10:08:05 -0400

    refactor(nodejs): Comport OllamaLlmProvider

--- packages/nodejs/src/index.ts
+++ packages/nodejs/src/index.ts
@@ -1,2 +1,3 @@
 export { NodejsVatWorkerService } from './kernel/VatWorkerService.ts';
 export { makeKernel } from './kernel/make-kernel.ts';
+export { makeOllamaLlmProvider } from './llm-provider.ts';
