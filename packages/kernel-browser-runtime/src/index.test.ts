e24bfcdbcd8c3843564b915cdebb98804e7fe0e4
Chip Morningstar <chip@fudco.com>
2025-07-22 17:55:57 -0700

    CI madness 11: shadow of CI madness

--- packages/kernel-browser-runtime/src/index.test.ts
+++ packages/kernel-browser-runtime/src/index.test.ts
@@ -8,7 +8,6 @@ describe('index', () => {
       'PlatformServicesClient',
       'PlatformServicesServer',
       'establishKernelConnection',
-      'initRemoteComms',
       'makeIframeVatWorker',
       'receiveUiConnections',
       'rpcHandlers',
