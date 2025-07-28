351754e047331f6f05a280b2eea72bd29b93596a
grypez <143971198+grypez@users.noreply.github.com>
2025-07-28 19:05:21 -0400

    feat(extension): Use llm vat power in sample-vat

--- packages/extension/src/vats/sample-vat.js
+++ packages/extension/src/vats/sample-vat.js
@@ -4,15 +4,15 @@ import { Far } from '@endo/marshal';
 /**
  * Build function for generic test vat.
  *
- * @param {unknown} _vatPowers - Special powers granted to this vat (not used here).
+ * @param {unknown} vatPowers - Special powers granted to this vat.
+ * @param {unknown} vatPowers.llm - An llm service.
  * @param {unknown} parameters - Initialization parameters from the vat's config object.
  * @param {unknown} _baggage - Root of vat's persistent state (not used here).
  * @returns {unknown} The root object for the new vat.
  */
-export function buildRootObject(_vatPowers, parameters, _baggage) {
+export function buildRootObject({ llm }, parameters, _baggage) {
   const name = parameters?.name ?? 'anonymous';
   console.log(`buildRootObject "${name}"`);
-
   return Far('root', {
     async bootstrap(vats) {
       console.log(`vat ${name} is bootstrap`);
@@ -26,5 +26,6 @@ export function buildRootObject(_vatPowers, parameters, _baggage) {
       console.log(message);
       return message;
     },
+    makeInstance: (archetype = 'fast') => llm.makeInstance({ archetype }),
   });
 }
