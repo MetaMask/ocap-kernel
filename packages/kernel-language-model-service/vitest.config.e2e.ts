795493a93ffcefb3da9cab3472697b65c712306a
grypez <143971198+grypez@users.noreply.github.com>
2025-08-12 11:00:02 -0400

    test(kernel-language-model-service): setup (mock) endoify

--- packages/kernel-language-model-service/vitest.config.e2e.ts
+++ packages/kernel-language-model-service/vitest.config.e2e.ts
@@ -1,4 +1,5 @@
 import { mergeConfig } from '@ocap/test-utils/vitest-config';
+import path from 'path';
 import { defineConfig, defineProject } from 'vitest/config';
 
 import defaultConfig from '../../vitest.config.ts';
@@ -10,6 +11,7 @@ export default defineConfig((args) => {
     defineProject({
       test: {
         name: 'kernel-language-model-service:e2e',
+        setupFiles: path.resolve(__dirname, '../kernel-shims/src/endoify.js'),
         include: ['./test/e2e/**/*.test.ts'],
         exclude: ['./src/**/*'],
       },
--- packages/kernel-language-model-service/vitest.config.ts
+++ packages/kernel-language-model-service/vitest.config.ts
@@ -1,4 +1,5 @@
 import { mergeConfig } from '@ocap/test-utils/vitest-config';
+import path from 'path';
 import { defineConfig, defineProject } from 'vitest/config';
 
 import defaultConfig from '../../vitest.config.ts';
@@ -10,6 +11,7 @@ export default defineConfig((args) => {
     defineProject({
       test: {
         name: 'kernel-language-model-service',
+        setupFiles: path.resolve(__dirname, '../kernel-shims/src/endoify.js'),
         exclude: ['./test/e2e/**'],
       },
     }),
