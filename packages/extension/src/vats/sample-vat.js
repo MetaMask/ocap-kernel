d7326d5739953066e85689b7b8a10b98f5fea46c
grypez <143971198+grypez@users.noreply.github.com>
2025-08-31 08:47:55 -0500

    feat(extension): Use kernel-platforms

--- packages/extension/src/vats/sample-vat.js
+++ packages/extension/src/vats/sample-vat.js
@@ -26,5 +26,9 @@ export function buildRootObject(_vatPowers, parameters, _baggage) {
       console.log(message);
       return message;
     },
+    async fetch(url) {
+      const response = await fetch(url);
+      return await response.json();
+    },
   });
 }
