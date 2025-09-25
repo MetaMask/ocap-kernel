a22c02fb9f342aaf390ef9ccd71e58c9e7565084
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2025-09-25 15:44:07 -0700

    fix: Fix broken import in build-constants.mjs

--- packages/extension/scripts/build-constants.mjs
+++ packages/extension/scripts/build-constants.mjs
@@ -13,7 +13,7 @@ export const kernelBrowserRuntimeSrcDir = path.resolve(
 );
 
 /**
- * @type {import('@ocap/repo-tools').PreludeRecord}
+ * @type {import('@ocap/repo-tools/vite-plugins').PreludeRecord}
  */
 export const trustedPreludes = {
   background: {
