96eefaa7d588c1624c46c576fa646fbb5ac2e93c
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2025-09-25 15:30:00 -0700

    chore: Add repo-tools and test-utils to all packages

--- packages/streams/vitest.config.ts
+++ packages/streams/vitest.config.ts
@@ -1,4 +1,4 @@
-import { mergeConfig } from '@ocap/test-utils/vitest-config';
+import { mergeConfig } from '@ocap/repo-tools/vitest-config';
 import { defineConfig, defineProject } from 'vitest/config';
 
 import defaultConfig from '../../vitest.config.ts';
