e12be1b1b246ea9535f17843e5e124c9312e5b69
Dimitris Marlagkoutsos <info@sirtimid.com>
2025-12-19 20:06:42 +0100

    close channel to release network resources

--- vitest.config.ts
+++ vitest.config.ts
@@ -130,7 +130,7 @@ export default defineConfig({
         'packages/kernel-ui/**': {
           statements: 97.57,
           functions: 97.29,
-          branches: 93.25,
+          branches: 93.26,
           lines: 97.57,
         },
         'packages/kernel-utils/**': {
@@ -158,10 +158,10 @@ export default defineConfig({
           lines: 22.22,
         },
         'packages/ocap-kernel/**': {
-          statements: 96.27,
+          statements: 96.4,
           functions: 98.55,
-          branches: 97.33,
-          lines: 96.27,
+          branches: 97.27,
+          lines: 96.4,
         },
         'packages/omnium-gatherum/**': {
           statements: 5.67,
