9cd75a67a7376166978867e1160669ff08e7e53b
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2026-01-12 10:52:24 -0800

    fix: Update yarn constraints

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -318,7 +318,7 @@ module.exports = defineConfig({
       if (isChildWorkspace) {
         workspace.unset('packageManager');
       } else {
-        expectWorkspaceField(workspace, 'packageManager', 'yarn@4.2.2');
+        expectWorkspaceField(workspace, 'packageManager', 'yarn@4.12.0');
       }
 
       // All packages must specify a minimum Node.js version of 20
